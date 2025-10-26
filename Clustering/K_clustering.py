8e03e1800ecd58601eda11305d5aec9f91ad7222
AkhilRobo <padidarapuakhilsai@yahoo.com>
2025-10-26 22:20:45 +0530

    K clustering

--- Clustering/K_clustering.py
+++ Clustering/K_clustering.py
@@ -6,24 +6,14 @@ import cv2
 def cluster_gen(n_clusters, pts_minmax=(10, 100), x_mult=(1, 4), y_mult=(1, 3), 
                 x_off=(0, 50), y_off=(0, 50)):
     
-    # n_clusters = number of clusters to generate
-    # pts_minmax = range of number of points per cluster 
-    # x_mult = range of multiplier to modify the size of cluster in the x-direction
-    # y_mult = range of multiplier to modify the size of cluster in the y-direction
-    # x_off = range of cluster position offset in the x-direction
-    # y_off = range of cluster position offset in the y-direction
-    
-    # Initialize some empty lists to receive cluster member positions
     clusters_x = []
     clusters_y = []
-    # Genereate random values given parameter ranges
     n_points = np.random.randint(pts_minmax[0], pts_minmax[1], n_clusters)
     x_multipliers = np.random.randint(x_mult[0], x_mult[1], n_clusters)
     y_multipliers = np.random.randint(y_mult[0], y_mult[1], n_clusters)
     x_offsets = np.random.randint(x_off[0], x_off[1], n_clusters)
     y_offsets = np.random.randint(y_off[0], y_off[1], n_clusters)
      
-    # Generate random clusters given parameter values
     for idx, npts in enumerate(n_points):
         
         xpts = np.random.randn(npts) * x_multipliers[idx] + x_offsets[idx]
@@ -31,37 +21,27 @@ def cluster_gen(n_clusters, pts_minmax=(10, 100), x_mult=(1, 4), y_mult=(1, 3),
         clusters_x.append(xpts)
         clusters_y.append(ypts)
     
-    # Return cluster positions
     return clusters_x, clusters_y
 
-# Generate some clusters!
 n_clusters = 7
 clusters_x, clusters_y = cluster_gen(n_clusters)
-# Convert to a single dataset in OpenCV format
 data = np.float32((np.concatenate(clusters_x), np.concatenate(clusters_y))).transpose()
 
-# Define k-means parameters
-# Number of clusters to define
+
 k_clusters = 7
-# Maximum number of iterations to perform
 max_iter = 10
-# Accuracy criterion for stopping iterations
 epsilon = 1.0
-# Define criteria in OpenCV format
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-# Call k-means algorithm on your dataset
 compactness, label, center = cv2.kmeans(data, k_clusters, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-# Define some empty lists to receive k-means cluster points
 kmeans_clusters_x = []
 kmeans_clusters_y = []
 
-# Extract k-means clusters from output
 for idx in range (k_clusters):
     kmeans_clusters_x.append(data[label.ravel()==idx][:,0])
     kmeans_clusters_y.append(data[label.ravel()==idx][:,1])
     
-# Plot up a comparison of original clusters vs. k-means clusters
+#Ploting
 fig = plt.figure(figsize=(12,6))
 plt.subplot(121)
 min_x = np.min(data[:, 0])
@@ -75,7 +55,7 @@ for idx, xpts in enumerate(clusters_x):
     plt.title('Original Clusters', fontsize=20)
 plt.subplot(122)
 for idx, xpts in enumerate(kmeans_clusters_x):
-    plt.plot(xpts, kmeans_clusters_y[idx], 'o')
+    plt.plot(xpts, kmeans_clusters_y[idx], 'x')
     plt.xlim(min_x, max_x)
     plt.ylim(min_y, max_y)
     plt.title('k-means Clusters', fontsize=20)
